9fdfc5d8e6f97c7f9b61bb02d438a6e5ee8227ac
bflife <zhuwei8483@163.com>
2024-05-02 18:51:33 +0800

    clearand18G.sh

--- clearand18G.sh
+++ clearand18G.sh
@@ -30,6 +30,6 @@ sleep 30
 echo "绑定id"
 ./titan-edge bind --hash=5B48F06A-52D3-4544-8640-48B0DCB99876 https://api-test1.container1.titannet.io/api/v2/device/binding 
 echo "设置容量"
-./titan-edge config set --storage-size 18GB 
+./titan-edge config set --storage-size 500GB 
 pkill -f "titan-edge daemon start"
 nohup ./titan-edge daemon start --init --url https://us-locator.titannet.io:5000/rpc/v0 &
