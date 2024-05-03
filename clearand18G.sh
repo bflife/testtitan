2be1e8b61ddec2fee1ad3a8ae97317105974af02
bflife <zhuwei8483@163.com>
2024-05-03 15:29:39 +0800

    clearand18G.sh

--- clearand18G.sh
+++ clearand18G.sh
@@ -11,7 +11,9 @@ docker rmi $(docker images -q)
 truncate -s 0 /var/lib/docker/containers/*/*-json.log
 
 # Clean /root directory
-find /root -type d -name 'titanedge*' -exec rm -rf {} +
+find /root -type d -name '.titanedge*' -exec rm -rf {} +
+find /root -type d -name 'titan*' -exec rm -rf {} +
+rm -rf /root/titan
 
 # Download and extract titan_v0.1.18_linux_amd64.tar.gz
 wget https://github.com/Titannet-dao/titan-node/releases/download/v0.1.18/titan_v0.1.18_linux_amd64.tar.gz
@@ -28,7 +30,7 @@ echo "执行程序"
 nohup ./titan-edge daemon start --init --url https://us-locator.titannet.io:5000/rpc/v0 &
 sleep 30
 echo "绑定id"
-./titan-edge bind --hash=5B48F06A-52D3-4544-8640-48B0DCB99876 https://api-test1.container1.titannet.io/api/v2/device/binding 
+./titan-edge bind --hash=39C42F62-DAEF-4543-A0DE-F8CE378EA78E https://api-test1.container1.titannet.io/api/v2/device/binding 
 echo "设置容量"
 ./titan-edge config set --storage-size 72GB 
 mkdir -p  /root/titan/storage
